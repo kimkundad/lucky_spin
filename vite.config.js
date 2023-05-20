35cc8938441ad19a569aceee1f1b8958727283c2
kimkundad <kim.kundad@gmail.com>
2023-05-21 02:29:23 +0700

    kim

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from 'path'
 export default defineConfig({
     plugins: [
         laravel([
-            'resource/scss/app.scss',
+            './resource/scss/app.scss',
             'resources/js/app.js',
         ]),
         vue(),
