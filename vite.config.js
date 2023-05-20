ef88d55d42456866b4089829c64978672abf39e4
kimkundad <kim.kundad@gmail.com>
2023-05-21 01:43:51 +0700

    kim

--- vite.config.js
+++ vite.config.js
@@ -12,8 +12,8 @@ export default defineConfig({
         vue(),
     ],
     server: {
-        port: 8000,
-     },
+        host: '127.0.0.1'
+      },
     resolve: {
         alias: {
             '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
